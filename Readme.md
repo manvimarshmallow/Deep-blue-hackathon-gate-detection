db681795b15230644d73054d46a59ede9ddf6f35
Manvi Bengani <manvib22@iitk.ac.in>
2024-01-16 14:33:09 +0530

    uploading final code

---
+++ Readme.md
@@ -0,0 +1,77 @@
+
+*Introduction:*
+The problem includes detecting and labeling an underwater gate from a provided dataset by developing a raw image processing algorithm without using ML or YOLO. The result can hence be utilized in the movement of AUV to go through the gate without colliding. The center coordinates of the gate will be useful to the navigation layer of AUV for passing through the gate.
+
+Approach and Explanation:
+The major techniques employed include :
+1) Image Enhancement/Noise Reduction: Histogram Equalization (CLAHE), Gamma Correction, White Balancing, Bilateral Filtering (Gaussian Blur while preserving edges)
+2) Edge Detection: Canny Edge Detector with manual thresholds
+3) Contour Detection & Filtering: Filtering using the aspect ratio of the bars of the
+gate
+4) Gate Line Detection: Probabilistic Hough Line Transform
+5) Polygon Approximation: Approximation of polygon formed by lines
+6) Bounding Box Creation: Creating a bounding box around the region containing
+the gate using heuristics involving distance thresholds.
+      Below Libraries Used:
+is the brief of libraries and functions used:
+➔ glob: Used for file path pattern matching.
+➔ os: Used for operating system-related operations.
+➔ cv2 (OpenCV): Main library for computer vision tasks. ➔ NumPy (np): Used for numerical operations.
+
+Functions:
+1. decrease_contrast(image, gamma=0.5)
+Decreases the contrast of the input image using a gamma correction.
+2. preprocess(src, im_resize=1)
+Resize the input image (src) by a factor of im_resize.
+
+ Apply noise reduction, histogram equalization, gamma correction, and adaptive thresholding to enhance image features.
+Detect edges using the Canny edge detector.
+Detect contours in the edge-detected image.
+Filter contours based on certain criteria (using functions is_gate_bar and is_gate_top).
+Create a binary image highlighting the filtered contours.
+Detect lines using the HoughLinesP function.
+Draw lines on a black image.
+Draw bounding boxes around detected objects based on lines.
+Display intermediate and final results using OpenCV.
+3. detect_edges(image)
+Applies the Canny edge detector to the input image.
+4. detect_contours(edges)
+Finds contours in the binary edge image.
+5. draw_lines(image, lines)
+Draws lines on a black image based on the input lines.
+6. detect_lines(edges)
+Applies the HoughLinesP algorithm to detect lines in the edge-detected image.
+7. is_gate_bar(c) and is_gate_top(c)
+Helper functions to filter contours based on whether it is a top part of the gate or the
+side portion.
+8. filter_contours(contours)
+Filters contours using the is_gate_bar and is_gate_top functions. It returns a list of valid contours i.e. the ones representing side and top of gate.
+
+ 9. create_contours_image(image, contours) Draws contours on a black image.
+10. reduce_noise(image)
+Applies a bilateral filter for noise reduction.
+11. draw_bounding_box(image, lines, distance_threshold=400, iteration_counter=0) Draws bounding boxes around detected objects based on lines.
+Performs filtering based on the distance between coordinates.
+Returns the modified image, cropped image, and a flag indicating whether a valid bounding box was found.
+12. main()
+The entry point of the script.
+Iterates through images in a specified folder, applies the preprocessing, and displays the results.
+
+Challenges and Solutions:
+1. Thedatasetcontainsimagesfromdifferentdistancesthatcanbecategorizedas far, moderate, and close; hence, it is challenging to find a mask common to all images as the threshold ranges are different for them. (A mask is choosing only those pixels you care about). An approach involving generating masks for detecting the vertical and horizontal bars using pixel values is insufficient to handle all kinds of gates since the “blueness” of an image varies exponentially with distance. A probable approach would involve creating all of these masks for all 3 types of gates (close, moderate, and far).
+2. Evenforimageswithinthesamecategory,thethresholdvalueshavean inflexible range that takes into consideration lines that are not part of the gate and are a part of the background. To overcome this and avoid manual tuning, we employed basic image enhancement schemes to improve the contrast between foreground and background using gamma correction and histogram equalization on the blue and green channels, as these are the prominent channels in underwater images.
+
+ 3. CannyEdgeDetector(withvaluessomewhattuned)withHoughTransformto detect straight lines, detects erroneous lines as well. A probable solution is to improve image enhancement and apply multiple noise reduction techniques to generate a better mask containing gate edges and gate bars solely.
+Performance analysis:
+We can visualize that with the heuristics developed; we are able to detect the region containing the gate always with performance varying based on the closeness of the gate.
+Gate centers were manually marked and compared with the ones obtained via the algorithm, with a ~20% error in the center coordinates on average.
+Conclusion:
+Overall, we can conclude that image enhancement involving improving contrast, lighting conditions, and segmenting the foreground and background is a crucial part of the pipeline. The efficiency of edge detection and contour detection is majorly affected by the image generated by the image enhancement code.
+Other heuristics that can be developed involve shortening the RoI (Region of Interest) by first doing a loose calculation of the position of the gate and then targeting specifically the cropped part of the image for better detection. Depth information, if possible from a depth camera, would also provide helpful information for the detection of gates.
+
+References for Future Work:
+
+1) https://github.com/CXH-Research/Underwater-Image-Enhancement (Underwater Image Enhancement)
+2) https://openaccess.thecvf.com/content_CVPR_2019/papers/Akkaynak_Sea-Thru _A_Method_for_Removing_Water_From_Underwater_Images_CVPR_2019_pap er.pdf (Image Enhancement to remove water background)
+    
+ 
---
+++ finalscript.py
@@ -0,0 +1,163 @@
+from glob import glob
+import os
+import cv2
+import numpy as np
+
+def decrease_contrast(image, gamma=0.5):
+    gamma_corrected = np.power(image / 255.0, gamma) * 255.0
+    gamma_corrected = np.uint8(gamma_corrected)
+    return gamma_corrected
+def white_balance(image):
+    # Assume the image is in BGR color space
+    result = cv2.xphoto.createSimpleWB()
+    balanced_image = result.balanceWhite(image)
+    return balanced_image
+def preprocess(src, im_resize=1):
+
+
+    im_dims = (int(src.shape[1]*im_resize), int(src.shape[0]*im_resize))
+    if im_resize != 1.0:
+        src=white_balance(src)
+        split = cv2.split(src)
+        kernel = (3,3)
+        sig = 1
+        temp = [reduce_noise(channel) for channel in split]
+        src = cv2.merge(temp)
+        src = cv2.resize(src, im_dims, cv2.INTER_CUBIC)
+    cv2.imshow("Original", src)
+    b,g,r = cv2.split(src)
+    clahe = cv2.createCLAHE(clipLimit=2.0, tileGridSize=(8, 8))
+    b_enhanced = clahe.apply(b)
+    g_enhanced = clahe.apply(g)
+    merged_image = cv2.merge([b_enhanced,g_enhanced,r])
+    gray_image = cv2.cvtColor(merged_image, cv2.COLOR_BGR2GRAY)
+    equalized_image = cv2.equalizeHist(gray_image)
+    equalized_image = reduce_noise(equalized_image)
+    range_image = np.uint8(np.minimum(np.uint32(equalized_image) + 10, 255))
+    gamma_image = decrease_contrast(range_image, 0.1)
+    gamma_image = np.float32(gamma_image)
+    gamma_image = ((gamma_image-np.amin(gamma_image))/(np.amax(gamma_image)-np.amin(gamma_image)))*255
+    gamma_image = np.uint8(gamma_image)
+    gamma_image = reduce_noise(gamma_image)
+    # gamma_image = cv2.GaussianBlur(gamma_image,(3,3),1.0)
+    block_size = 11  
+    constant = 5 
+    adaptive_thresholded_image = cv2.adaptiveThreshold(
+        gamma_image, 255, cv2.ADAPTIVE_THRESH_GAUSSIAN_C, cv2.THRESH_BINARY_INV, block_size, constant
+    )
+    edges = detect_edges(gamma_image)
+    # cv2.imshow("Test_Equalized", equalized_image)
+    # cv2.imshow("Test_Gamma", gamma_image)
+    # cv2.imshow("Test_Adapt", adaptive_thresholded_image)
+
+    contours = detect_contours(edges)
+    filtered = filter_contours(contours)
+    contours_image = create_contours_image(edges.copy(), filtered)
+
+    lines = detect_lines(contours_image)
+    draw_lines(contours_image, lines)
+    final, cropped,  _ = draw_bounding_box(src, lines)
+    # cv2.imshow("Edges", edges)
+    # cv2.imshow("Filtered", contours_image)
+    cv2.imshow("Final", final)
+    # cv2.imshow("Cropped", cropped)
+    # cv2.waitKey(0)
+    return cropped, _
+
+def detect_edges(image):
+    edges = cv2.Canny(image, 80, 150)
+    return edges
+
+def detect_contours(edges):
+    contours, _ = cv2.findContours(edges, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
+    return contours
+
+def draw_lines(image, lines):
+    image = np.zeros_like(image)
+    for line in lines:
+        x1, y1, x2, y2 = line[0]
+        cv2.line(image, (x1, y1), (x2, y2), (255, 255, 255), 2)
+    cv2.imshow("Lines", image)
+    return image
+
+def detect_lines(edges):
+    lines = cv2.HoughLinesP(edges, 1, np.pi / 180, threshold=100, minLineLength=100, maxLineGap=8) 
+    return lines
+
+def is_gate_bar(c):
+    peri = cv2.arcLength(c, True)
+    approx = cv2.approxPolyDP(c, 0.05 * peri, True) 
+    (x, y, w, h) = cv2.boundingRect(approx) 
+
+    if h < (3.5*w): 
+        return False, (x, y, w, h)
+    return True, (x, y, w, h)
+
+def is_gate_top(c):
+    peri = cv2.arcLength(c, True)
+    approx = cv2.approxPolyDP(c, 0.05 * peri, True) 
+    (x, y, w, h) = cv2.boundingRect(approx) 
+    if w < h: 
+        return False, (x, y, w, h)
+    return True, (x, y, w, h)
+
+def filter_contours(contours):
+    filtered_contours = [c for c in contours if is_gate_bar(c)[0]]
+    top_contours = [c for c in contours if is_gate_top(c)[0]]
+    for c in top_contours:
+        filtered_contours.append(c)
+    return filtered_contours
+
+def create_contours_image(image, contours):
+    contours_image = np.zeros_like(image)
+    cv2.drawContours(contours_image, contours, -1, (255, 255, 255), thickness=2)
+    return contours_image
+
+
+def reduce_noise(image):
+    # Apply bilateral filter for noise reduction
+    denoised_image = cv2.bilateralFilter(image, d=9, sigmaColor=20, sigmaSpace=3)
+    return denoised_image
+
+def draw_bounding_box(image, lines, distance_threshold=400, iteration_counter=0):
+    if lines is not None and len(lines) > 0:
+        coordinates = np.array([line[0] for line in lines])
+        if len(coordinates) > 0:
+            try:
+                distances = np.linalg.norm(np.diff(coordinates, axis=0), axis=1)
+                valid_indices = np.where(distances < distance_threshold)[0]
+                filtered_coordinates = coordinates[valid_indices]      
+                if len(filtered_coordinates) > 0:
+                    x_min, y_min = np.min(filtered_coordinates, axis=0)[:2]
+                    x_max, y_max = np.max(filtered_coordinates, axis=0)[2:]       
+                    center_x = (x_min + x_max) // 2
+                    center_y = (y_min + y_max) // 2
+
+                    bounding_box_area = (x_max - x_min) * (y_max - y_min)
+                    image_area = image.shape[0] * image.shape[1] // 2
+                    cropped_image = np.zeros_like(image)
+                    pos = False
+                    if bounding_box_area > image_area:
+                        cropped_image = image[y_min:y_max, x_min:x_max]
+                        cropped_image = cropped_image.copy()
+                        pos = True
+                    cv2.rectangle(image, (x_min, y_min), (x_max, y_max), (0, 255, 0), 2)
+                    cv2.circle(image, (center_x, center_y), 5, (0, 0, 255), -1)
+                    cv2.putText(image,str(center_x)+","+str(center_y),(center_x,center_y),cv2.FONT_HERSHEY_PLAIN,1,(255,255,255),1)
+            except cv2.error as e:
+                print("Error in drawing bounding box:", e)
+    return image, cropped_image, pos
+
+def main():
+
+    folder_path =  r"C:\Users\visha\Desktop\images"
+
+    for path in os.listdir(folder_path):#loop to read one image at a time 
+        image = os.path.join(folder_path, path)
+        img, more = preprocess(cv2.imread(image),0.4)
+        # if more:
+        #     img,_ = preprocess(img,1.0)
+        cv2.waitKey(0)
+        cv2.destroyAllWindows()
+
+main()
